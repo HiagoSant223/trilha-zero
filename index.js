9b35f42958c043c10af59bf651c1226abe776337
Hiago Oliveira <hiagosantos223@gmail.com>
2024-02-03 18:50:08 -0300

    arquivo Js

---
+++ index.js
@@ -0,0 +1,16 @@
+var xhr = new XMLHttpRequest();
+var resultado = null;
+
+xhr.open('GET', 'https://restcountries.com/v3.1/name/brazil');
+xhr.send();
+
+xhr.onreadystatechange = funcion(); {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            resultado = JSON.parse(xhr.response);
+
+            console.log(resultado)
+
+            console.log(s)
+    }
+
+}
